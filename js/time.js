72fa7bc896fdaea6cf497a9c41f88c34821ce351
Subrat Mallick <m.subrat198470@gmail.com>
2022-06-19 22:43:20 +0530

    as

--- js/time.js
+++
@@ -1,8 +0,0 @@
-const displayTime = document.querySelector("#time");
-function showTime() {
-    let time = new Date();
-    displayTime.innerText = time.toLocaleTimeString("en-US", { hour12: false });
-    setTimeout(showTime, 1000);
-}
-
-showTime();
